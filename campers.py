8933c590b20a3fde511504db1a1304812b97dc1c
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-07-11 12:32:44 -0500

    Actualizacion 3

--- campers.py
+++ campers.py
@@ -1,20 +1,27 @@
 import os
+import core
 import acudientes
+def LoadInfoCampers():
+    global diccCampers
+    if (core.checkFile("campers.json")):
+        diccCampers = core.LoadInfo("campers.json")
+    else:
+        core.crearInfo("campers.json",diccCampers)
 def MainMenu():
     os.system("clear")
     mainMenu = True
     os.system("clear")
     print('+','-'*55,'+')
-    print("|{:^16}{}{:^17}|".format(' ','ADMINISTRACION DE COMPRAS',' '))
+    print("|{:^16}{}{:^16}|".format(' ','ADMINISTRACION DE CAMPERS',' '))
     print('+','-'*55,'+')
     print("1. Registrar camper")
     print("2. Buscar camper")
     print("3. Eliminar camper")
     print("4. Editar camper")
     print("5. Regresar menu principal")
-    opcion =int(input(":)_"))
+    opcion =int(input("Opcion: "))
     if (opcion == 1):
-        pass
+        core.registroCampers()
     elif (opcion == 2):
         pass
     elif (opcion == 3):
--- principal.py
+++ principal.py
@@ -17,19 +17,18 @@
 # La informacion ingresada debe ser almacenada de forma local
 import os
 import campers
- 
 if __name__=="__main__":
     menu=True
-    opcion=""
+    datos={'data':[]}
     while menu:
         os.system("clear")
         print('+','-'*55,'+')
-        print("|{:^20}{}{:^24}|".format(' ','Menu Principal',' '))
+        print("|{:^20}{}{:^23}|".format(' ','Menu Principal',' '))
         print('+','-'*55,'+')
         print("1. Registrar campers")
         print("2. Salir")
-        opcion=input("Opcion: ")
-        if opcion=="1":
+        opcion=int(input("Opcion: "))
+        if opcion==1:
             campers.MainMenu()
-        elif opcion=="2":
+        elif opcion==2:
             menu=False
